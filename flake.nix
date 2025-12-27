88c21ebbdb92bd342f34d72ffa0a8eb52d834529
Sebastian Di Luzio <sebastian@diluz.io>
2025-12-27 16:24:02 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "EOY project development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.packages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+            pnpm
+          ];
+
+          shellHook = ''
+            node --version
+            pnpm --version
+          '';
+        };
+      }
+    );
+}
